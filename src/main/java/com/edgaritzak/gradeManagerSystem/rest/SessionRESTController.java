00f4598afb67989c7c259648f89a9dcdbd8b0a84
Edgar Itzak Sanchez Rogers <109997241+ItzakSanchez@users.noreply.github.com>
2024-09-11 17:32:17 -0600

    Spring Security & Login Controller

---
+++ src/main/java/com/edgaritzak/gradeManagerSystem/rest/SessionRESTController.java
@@ -0,0 +1,33 @@
+package com.edgaritzak.gradeManagerSystem.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SessionRESTController {
+
+	
+	
+	@GetMapping("/login")
+	public String siteHome(Model model, @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+		if (error != null) {
+		model.addAttribute("error", "Invalid username or password.");
+		}
+		if (logout != null) {
+		model.addAttribute("logout", "You have been logged out.");
+		}
+		return "login";
+	}
+	
+	
+	@ResponseBody
+	@GetMapping("/hi")
+	public String hello() {
+		return "Hello";
+	}
+}
